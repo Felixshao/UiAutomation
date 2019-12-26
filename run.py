648cf2c884aade84421b32da4d33d00b59b99b5c
Felixshao <50865269+Felixshao@users.noreply.github.com>
2019-12-26 16:53:36 +0800

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ from common.runReport import run_report
 from common.sendEmail import send_annex_email
 
 if __name__ == '__main__':
-
+    
+    # github edit, pycharm pull
     # run_report().run_html()     # 运行用例并生成报告
     run_report().more_report()
     # run_report().thread_run_html()
