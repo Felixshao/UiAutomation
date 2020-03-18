5a48296b3ecac05a44b6ecd362c120bde5f71f68
Felixshao <shaoyufei1234@163.com>
2020-03-18 14:38:29 +0800

    3.18 14.38修改stock.xlsx格式

--- stock_recording.py
+++ stock_recording.py
@@ -5,7 +5,7 @@ from config.getProjectPath import get_project_path
 
 
 path = get_project_path()
-file = os.path.join(path, 'file', 'stock.xlsx')
+file = os.path.join(path, 'file', 'stock2.xlsx')
 fill_name = 'stock_interface.xlsx'
 sheet_name = 'stock'
 stock = operateExcel(file_name=fill_name, sheet_name=sheet_name).get_excel_list()
@@ -34,7 +34,6 @@ def process():
     pool.join()
     for i in range(q.qsize()):
         stock_result.append(q.get())
-    print(stock_result)
     prices = {}
     num = 0
     for i in stock_result:
@@ -55,18 +54,25 @@ def process():
             shell = table.create_sheet(i)
             shell.cell(1, 1, i)
             shell.cell(2, 1, '时间')
-            shell.cell(2, 2, '详情')
+            shell.cell(2, 2, '当前')
+            shell.cell(2, 3, '最高')
+            shell.cell(2, 4, '最低')
+            shell.cell(2, 5, '今开')
+            shell.cell(2, 6, '昨收')
+            shell.cell(2, 7, '幅度')
         else:
             shell = table[i]
         row = shell.max_row     # max_row获取最大行数
         # col = table.max_column  # max_row获取最大列数
-        shell.cell(row+1, 1, t)
-        shell.cell(row+1, 2, str(prices[i]))
+        shell.cell(row + 1, 1, t)
+        col = 2
+        for n in prices[i]:
+            shell.cell(row+1, col, str(prices[i][n]))
+            col += 1
     table.save(file)
     print('成功写入:{}'.format(file))
 
 
 if __name__ == '__main__':
     process()
-
 
