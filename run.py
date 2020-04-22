4375b0e40fc23417144d272c4ac9e9d844eb4ae4
Felixshao <50865269+Felixshao@users.noreply.github.com>
2020-04-22 19:02:30 +0800

    Update run.py

--- run.py
+++ run.py
@@ -13,8 +13,9 @@ if __name__ == '__main__':
     t1 = time.time()
     clear.clear_screenshot()    # 清空截图
     clear.clear_report()  # 清空报告
+    run_report().run_html()
     # run_report().moreProcess_report()   # 多进程运行用例并生成报告
-    run_report().pool_report()  # 多进程运行用例并生成报告
+#     run_report().pool_report()  # 多进程运行用例并生成报告
     # run_report().run_html()     # 运行用例并生成报告
     send_annex_email()          # 发送报告邮件
     print('总耗时:', time.time() - t1)
