0a1c1493ca5e5a36da922cc92ec8029e02eec644
Felixshao <shaoyufei1234@163.com>
2020-04-22 16:13:25 +0800

    2020.4.22 提交linux调试代码

--- linux_tiaos.py
+++ linux_tiaos.py
@@ -1,4 +1,8 @@
 from selenium import webdriver
+from pyvirtualdisplay import Display
+
+display = Display(visible=0, size=(900, 800))
+display.start()
 
 driver = webdriver.Chrome()
 driver.get('www.baidu.com')
