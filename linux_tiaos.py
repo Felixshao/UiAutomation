0cefad8a4d8cb4cd4124e83fc7804b5e4adcd93b
Felixshao <shaoyufei1234@163.com>
2020-04-22 14:45:36 +0800

    2020.4.22 提交linux调试代码

--- linux_tiaos.py
+++ linux_tiaos.py
@@ -1,4 +1,7 @@
-from selenium import webdriver
+# from selenium import webdriver
+#
+# driver = webdriver.Chrome()
+# driver.get('www.baidu.com')
 
-driver = webdriver.Chrome()
-driver.get('www.baidu.com')
+import platform
+print(platform.python_version())
